5f99cf67d3aff14c76031c529756285ad3b400d8
sphfs196 <sphfs196@gmail.com>
2016-04-04 13:25:34 +0800

    ~

--- Sudoku.cpp
+++ Sudoku.cpp
@@ -347,34 +347,54 @@ bool Sudoku::solveS(int s[9][9])
 bool Sudoku::checkU(int s[9][9],int &row,int &col)
 {
 	for(row=0;row<9;row++)
+	{
 		for(col=0;col<9;col++)
+		{
 			if(s[row][col]==0)
+			{
 				return true;
+			}
+		}
+	}
 	return false;
 }
 
 bool Sudoku::checkRow(int s[9][9],int row,int num)
 {
 	for(int col=0;col<9;col++)
+	{
 		if(s[row][col]==num)
+		{
 			return true;
+		}
+	}
 	return false;
 }
 
 bool Sudoku::checkCol(int s[9][9],int col,int num)
 {
 	for(int row=0;row<9;row++)
+	{
 		if(s[row][col]==num)
+		{
 			return true;
+		}
+	}
 	return false;
 }
 
 bool Sudoku::checkBox(int s[9][9],int rowBegin,int colBegin,int num)
 {
 	for(int row=0;row<3;row++)
+	{
 		for(int col=0;col<3;col++)
+		{
 			if(s[row+rowBegin][col+colBegin]==num)
+			{
 				return true;
+			}
+		}
+	}
 	return false;
 }
 
